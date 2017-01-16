2a4b3a8b34431b3b8f317dfe549d57be108b518b
Sezgin GÜL <sezgingul@outlook.com.tr>
2017-01-16 02:24:21 +0200

    visual basic code

---
+++ Robot Control Program/Robot Control Program/Form1.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.Teal
+        Me.Button10.Location = New System.Drawing.Point(380, 200)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(66, 51)
+        Me.Button10.TabIndex = 92
+        Me.Button10.Text = "Left Backward"
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.Teal
+        Me.Button9.Location = New System.Drawing.Point(540, 200)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(72, 51)
+        Me.Button9.TabIndex = 91
+        Me.Button9.Text = "Right Backward"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.Teal
+        Me.Button8.Location = New System.Drawing.Point(540, 79)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(72, 47)
+        Me.Button8.TabIndex = 90
+        Me.Button8.Text = "Right Forward"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Teal
+        Me.Button7.Location = New System.Drawing.Point(380, 79)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(66, 47)
+        Me.Button7.TabIndex = 89
+        Me.Button7.Text = "Left Forward"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.ForeColor = System.Drawing.Color.Red
+        Me.TextBox1.Location = New System.Drawing.Point(415, 290)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(145, 20)
+        Me.TextBox1.TabIndex = 88
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Red
+        Me.Button6.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button6.Location = New System.Drawing.Point(591, 283)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(64, 32)
+        Me.Button6.TabIndex = 86
+        Me.Button6.Text = "LED OFF"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Green
+        Me.Button5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button5.Location = New System.Drawing.Point(323, 283)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(64, 32)
+        Me.Button5.TabIndex = 87
+        Me.Button5.Text = "LED ON"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.GhostWhite
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Tai Le", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Teal
+        Me.Label1.Location = New System.Drawing.Point(35, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(316, 32)
+        Me.Label1.TabIndex = 85
+        Me.Label1.Text = "Arduino Robot Control"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Teal
+        Me.GroupBox1.Controls.Add(Me.Button12)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Button13)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.White
+        Me.GroupBox1.Location = New System.Drawing.Point(41, 149)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(260, 114)
+        Me.GroupBox1.TabIndex = 94
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Serial Connection"
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.Red
+        Me.Button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button12.ForeColor = System.Drawing.Color.White
+        Me.Button12.Location = New System.Drawing.Point(160, 66)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(87, 23)
+        Me.Button12.TabIndex = 79
+        Me.Button12.Text = "Disconnect"
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(6, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 75
+        Me.Label2.Text = "BaudRate"
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.Color.Green
+        Me.Button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button13.ForeColor = System.Drawing.Color.White
+        Me.Button13.Location = New System.Drawing.Point(160, 23)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(87, 23)
+        Me.Button13.TabIndex = 78
+        Me.Button13.Text = "Connect"
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.BackColor = System.Drawing.Color.White
+        Me.ComboBox1.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(67, 25)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(83, 21)
+        Me.ComboBox1.TabIndex = 76
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(8, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 74
+        Me.Label3.Text = "Select Port"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(67, 66)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(87, 21)
+        Me.ComboBox2.TabIndex = 77
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.SystemColors.Control
+        Me.Button11.BackgroundImage = Global.Robot_Control_Program.My.Resources.Resources.Stop_sign
+        Me.Button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button11.Location = New System.Drawing.Point(452, 132)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(82, 62)
+        Me.Button11.TabIndex = 93
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Yellow
+        Me.Button4.BackgroundImage = Global.Robot_Control_Program.My.Resources.Resources.okalt
+        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button4.Location = New System.Drawing.Point(452, 200)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(82, 74)
+        Me.Button4.TabIndex = 81
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.Button3.BackColor = System.Drawing.Color.Yellow
+        Me.Button3.BackgroundImage = Global.Robot_Control_Program.My.Resources.Resources.oksag
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button3.ForeColor = System.Drawing.Color.Black
+        Me.Button3.Location = New System.Drawing.Point(540, 132)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(93, 62)
+        Me.Button3.TabIndex = 82
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.Button2.BackColor = System.Drawing.Color.Yellow
+        Me.Button2.BackgroundImage = Global.Robot_Control_Program.My.Resources.Resources.okust
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button2.Location = New System.Drawing.Point(452, 52)
+        Me.Button2.Name = "Button2"
+        Me.Button2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Button2.Size = New System.Drawing.Size(82, 74)
+        Me.Button2.TabIndex = 83
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Yellow
+        Me.Button1.BackgroundImage = Global.Robot_Control_Program.My.Resources.Resources.oksol
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.Location = New System.Drawing.Point(353, 132)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 62)
+        Me.Button1.TabIndex = 84
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.Red
+        Me.Button14.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button14.Location = New System.Drawing.Point(41, 274)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(260, 50)
+        Me.Button14.TabIndex = 95
+        Me.Button14.Text = "EXIT"
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(680, 382)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Robot Control"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+
+End Class
---
+++ Robot Control Program/Robot Control Program/Form1.vb
@@ -0,0 +1,251 @@
+﻿'Software development By Sezgin GÜL
+'Personal site : www.sezgingul.com 
+'Blog : www.robimek.com
+Imports System.IO
+Imports System
+Imports System.ComponentModel
+Imports System.Threading
+Imports System.IO.Ports
+
+
+
+
+Public Class Form1
+    Dim myPort As Array
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+    Shared _continue As Boolean
+    Shared _serialPort As SerialPort
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If (e.KeyCode = Keys.A) Then
+            Button1.PerformClick()
+            Button1.BackColor = Color.Red
+            TextBox1.Text = "Going left"
+
+        Else
+            Button1.BackColor = Color.Yellow
+
+
+        End If
+        If (e.KeyCode = Keys.D) Then
+            Button3.PerformClick()
+            Button3.BackColor = Color.Red
+            TextBox1.Text = "Going right"
+        Else
+            Button3.BackColor = Color.Yellow
+
+
+        End If
+        If (e.KeyCode = Keys.W) Then
+            Button2.PerformClick()
+            Button2.BackColor = Color.Red
+            TextBox1.Text = "Going forward"
+        Else
+            Button2.BackColor = Color.Yellow
+        End If
+        If (e.KeyCode = Keys.S) Then
+            Button4.PerformClick()
+            Button4.BackColor = Color.Red
+            TextBox1.Text = "Going backward"
+
+
+        Else
+            Button4.BackColor = Color.Yellow
+        End If
+        If (e.KeyCode = Keys.F) Then
+            Button5.PerformClick()
+            Button5.BackColor = Color.Green
+            Button6.BackColor = Color.White
+        End If
+        If (e.KeyCode = Keys.G) Then
+            Button6.PerformClick()
+            Button6.BackColor = Color.Red
+            Button5.BackColor = Color.White
+        End If
+        If (e.KeyCode = Keys.Q) Then
+            Button7.PerformClick()
+            Button7.BackColor = Color.Red
+            TextBox1.Text = "Going left forward "
+        Else
+            Button7.BackColor = Color.Teal
+        End If
+        If (e.KeyCode = Keys.E) Then
+            Button8.PerformClick()
+            Button8.BackColor = Color.Red
+            TextBox1.Text = "Going right forward "
+        Else
+            Button8.BackColor = Color.Teal
+        End If
+        If (e.KeyCode = Keys.X) Then
+            Button9.PerformClick()
+            Button9.BackColor = Color.Red
+            TextBox1.Text = "Going left backward"
+        Else
+            Button9.BackColor = Color.Teal
+        End If
+        If (e.KeyCode = Keys.Z) Then
+            Button10.PerformClick()
+            Button10.BackColor = Color.Red
+            TextBox1.Text = "Going right backward"
+        Else
+            Button10.BackColor = Color.Teal
+        End If
+        If (e.KeyCode = Keys.Space) Then
+            Button11.PerformClick()
+            Button11.BackColor = Color.Red
+            TextBox1.Text = "Stopping"
+        Else
+            Button11.BackColor = Color.Red
+        End If
+    
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        SerialPort1.WriteLine("W")
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        SerialPort1.WriteLine("A")
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+
+        SerialPort1.WriteLine("D")
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+
+        SerialPort1.WriteLine("S")
+
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        myPort = IO.Ports.SerialPort.GetPortNames()
+        ComboBox2.Items.Add(9600)
+        ComboBox2.Items.Add(19200)
+        ComboBox2.Items.Add(38400)
+        ComboBox2.Items.Add(57600)
+        ComboBox2.Items.Add(115200)
+        For i = 0 To UBound(myPort)
+            ComboBox1.Items.Add(myPort(i))
+        Next
+        ComboBox1.Text = ComboBox1.Items.Item(0)
+        ComboBox2.Text = ComboBox2.Items.Item(0)
+        Button12.Enabled = False
+
+    End Sub
+    Private Sub btnConnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        SerialPort1.PortName = ComboBox1.Text
+        SerialPort1.BaudRate = ComboBox2.Text
+        SerialPort1.Parity = IO.Ports.Parity.None
+        SerialPort1.StopBits = IO.Ports.StopBits.One
+        SerialPort1.DataBits = 8
+        SerialPort1.Open()
+        Button13.Enabled = False
+        Button12.Enabled = True
+    End Sub
+
+    Private Sub btnDisconnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        SerialPort1.Close()
+        Button13.Enabled = True
+        Button12.Enabled = False
+    End Sub
+
+
+
+    Private Sub cmbPort_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If SerialPort1.IsOpen = False Then
+            SerialPort1.PortName = ComboBox1.Text
+        Else
+            MsgBox("Change the connection when it is off !", vbCritical)
+        End If
+    End Sub
+ 
+    Private Sub cmbBaud_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox2.SelectedIndexChanged
+        If SerialPort1.IsOpen = False Then
+            SerialPort1.BaudRate = ComboBox2.Text
+        Else
+            MsgBox("Change the connection when it is off !", vbCritical)
+        End If
+    End Sub
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        SerialPort1.WriteLine("R")
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+
+        SerialPort1.WriteLine("T")
+
+
+    End Sub
+
+
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        SerialPort1.WriteLine("Q")
+
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+
+
+        SerialPort1.WriteLine("E")
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+
+
+        SerialPort1.WriteLine("X")
+
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+
+        SerialPort1.WriteLine("Z")
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+
+
+        SerialPort1.WriteLine("C")
+
+    End Sub
+
+
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If SerialPort1.IsOpen = True Then
+            SerialPort1.Close()
+            End
+
+        Else
+            End
+
+        End If
+    End Sub
+End Class
